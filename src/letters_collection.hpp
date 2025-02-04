49ddc0b1ded73b6ae179cf9f5b21363daa14c443
Raphaël HENG p2200092 <raphael.heng@etu.univ-lyon1.fr>
2025-02-04 15:08:00 +0100

    beginning letters collection

---
+++ src/letters_collection.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "letter.hpp"
+#include <vector>
+
+class LettersCollection {
+    private:
+        std::vector<Letter> letters_collection;
+    public:
+        LettersCollection(); 
+};
