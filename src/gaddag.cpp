7ae872a7f744ec9a67899cd4a6fa7aaa36a652ff
Raphaël HENG p2200092 <raphael.heng@etu.univ-lyon1.fr>
2025-02-21 22:06:54 +0100

    debut refacto class Gaddag, vector<tuple> into unordered_map

--- src/gaddag.cpp
+++ src/gaddag.cpp
@@ -2,10 +2,10 @@
 
 Gaddag::Gaddag() 
 {
-    bIsFinal = false;
+    isFinal = false;
 }
 
-vector<string> Gaddag::decompose(string word) 
+vector<string> Gaddag::decompose(const string& word) 
 {
     vector<string> decompositions;
     for (int i = 0; i < word.size(); i++)
@@ -19,71 +19,20 @@ vector<string> Gaddag::decompose(string word)
     return (decompositions);
 }
 
-tuple<bool, unsigned int> isLetterInSubGaddag(char letter) 
+void Gaddag::insertDecomposition(const string& decomposition) 
 {
-    for (int i = 0; i < nextSubGaddag.size(); i++) 
-    {
-        if (get<1>(nextSubGaddag.at(i)) == letter) 
-        {
-            return new_tuple(true, i);
-        }
-    }
-    return new_tuple(false, 0);
-}
-
-void Gaddag::addGaddag(const Gaddag& newGaddag, char letter) 
-{
-    tuple<Gaddag, char> new_tuple = make_tuple(newGaddag, letter);
-    nextGaddags.push_back(new_tuple);
-}
-
-void Gaddag::insert(string word)
-{
-    /*while (word == ""){
-        //Vérif si lettre pas déjà dans gaddag suivant
-        //Si c'est le cas on fait un appel de insert sur le gaddag existant avaec le reste du mot
-        //Sinon créer le gaddag et faire un insert sur le gaddag avec le reste du mot
-        //insert(word.substr( 1, word.size() ))
-    }
-    insert("reste du mot");*/
-
-    if (word == "") 
-    {
-        bIsFinal = true;
-        return;
-    }
-
-    string subword = word.substr(1, word.size() - 1);
-    Gaddag newGaddag;
-
-    if (nextGaddags.size() == 0) 
-    {
-        newGaddag.insert(subword);
-        addGaddag(newGaddag, word[0]);
-        return;
-    }
-
-    tuple<bool, unsigned int> bIsLetterInSubGaddagAndIndex = isLetterInSubGaddag(word[0]);
     
-    if (get<0>(bIsLetterInSubGaddagAndIndex)) 
-    {
-        nextGaddags.at(get<1>(bIsLetterInSubGaddagAndIndex)).insert(subword);
-        return;
-    } 
-    
-    newGaddag.insert(subword);
-    addGaddag(newGaddag, word[0]);
 }
 
-void Gaddag::insertWord(string word)
+void Gaddag::insertWord(const string& word)
 {
-    for (string str : decomp(word))
+    for (string str : decompose(word))
     {
         insert(str);
     }
 }
 
-void Gaddag::research(string word)
+void Gaddag::checkIfInGaddag(const string& word)
 {
     //si le mot est vide on renvoie le bIsFinal car si il n'est pas final on renvoie false sinon si le mot est fini et final on renvoie true
     if (word.empty())
--- src/gaddag.hpp
+++ src/gaddag.hpp
@@ -1,7 +1,6 @@
 #pragma once
 
-#include <vector>
-#include <tuple>
+#include <unordered_map>
 #include <string>
 #include <algorithm>
 
@@ -10,16 +9,9 @@ using namespace std;
 class Gaddag
 {
     private:
-        bool bIsFinal;
-        vector<tuple<Gaddag, char>> nextGaddags;
+        bool isFinal = false;
+        unordered_map<char, Gaddag> gaddags;
 
-        /**
-         * @brief Add a gaddag to nextGaddags vector
-         * @param newGaddag : newGaddag to add
-         * @param letter : Letter of the vertex connecting current Gaddag
-         *     to the new one
-         */
-        void addGaddag(const Gaddag& newGaddag, char letter);
     public:
         /**
          * @brief Gaddag's class constructor
@@ -29,45 +21,35 @@ class Gaddag
         /**
          * @brief Auxilary function to insert one decomposition of a word in
          *     the gaddag
-         * @param word : the decomposition to insert
+         * @param decomposition : the decomposition to insert
          */
-        void insert(string word);
+        void insertDecomposition(const string& decomposition);
 
         /**
          * @brief Get all decomposition of a word
          * @param word : The word to decompose
          * @return The vector of all the decompositions 
          */
-        vector<string> decompose(string word);
+        vector<string> decompose(const string& word);
 
         /**
          * @brief Main function to insert a word in the gaddag
-         * @param word : The word to insert
+         * @param word : The word to insert (all decompositions)
          */
-        void insertWord(string word);
+        void insertWord(const string& word);
 
         /**
          * @brief Look if a word is in the gaddag
          * @param word : The word to look for
          * @return True if the word is found
          */
-        bool research(string word);
+        bool checkIfInGaddag(const string& word);
 
         /**
          * @brief Overloading of the = operator for Gaddag
          * @return A Gaddag
          */
         Gaddag operator=(const Gaddag& gaddag);
-
-        /**
-         * @brief Check if the letter exists in the gaddag
-         * @param letter : The letter to look for
-         * @return A tuple<bool, unsigned int> : 
-         *     bool : If the letter has been found
-         *     unsigned int : The index of the subgaddag the letter has been 
-         *                    found in
-         */
-        tuple<bool, unsigned int> isLetterInSubGaddag(char letter);
 
         /**
          * @brief Display the gaddag
