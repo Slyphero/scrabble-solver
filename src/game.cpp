59ddaed629d1f0ae2ec308bea8b0164b3352644e
DUSSART--RABILLOUD BASTIEN p2204374 <p2204374@B761PC1208.univ-lyon1.fr>
2025-04-01 14:31:51 +0200

    pseudo algo validé

--- src/game.cpp
+++ src/game.cpp
@@ -8,38 +8,40 @@ parcours droite ou bas après + on teste de placer les lettre du joueur
 */
 
 /*
-coup(Board currentBoard, Spot currentLocation, Direction currentDirection, Player player, struct save){
-    if (Board(currentLocation) == 0){
-        while((0 <= l || l >= 14) && (0<= c || c >= 14)){
+
+
+coup(Board currentBoard, Spot currentLocation, Direction currentDirection, Player player,gaddag, struct save){
+    if (Board(currentLocation) == 0//la case ne contient pas de lettre){
+        if((0 <= l || l >= 14) && (0<= c || c >= 14)){
             for(int i = 0; i < player.inv.size(); i++){
                 verifier si gaddag[lettreJouer] existe{
-                    save.letter += score lettre * coef case
-                    save.multiple *= multiple mot
-                    if (gaddag est terminal){
+                   
+                    if (gaddag est terminal puis case suivante vide){
                         save état courant
-                        save.score *= save.multiple;
                     }
-                    coup(Boardmodif, currentLocation + direction, currentDirection, player.inv - lettre jouer, save);
+                    coup(Boardmodif, currentLocation + direction, currentDirection, player.inv - lettre jouer,gaddag, save);
                 }
+            }
+            if (Gaddag["+"] existe est case suivante vide){
+                coup (currentBoard, save.location, !direction, player, save);
             }
         }  
     }
     else {
-        save.letter += score lettre * coef case
-        save.multiple *= multiple mot
-        coup(currentBoard, currentLocation + direction, currentDirection, player, save);
+       
+        coup(currentBoard, currentLocation + direction, currentDirection, player, gaddag, save);
     }
-    if (Gaddag["+"] existe){
-        coup (currentBoard, save.location, !direction, player, save);
-    }
+    
 }
 
 autrefonction(){
     parcours le tab de (8,8) en allant sur les voisin non vide{
-        coup(Board,spotActuelle,LEFT, joueur, save);
-        coup(Board,spotActuelle,TOP, joueur, save);
+        coup(Board,spotActuelle,LEFT, joueur,gaddag, save);
+        coup(Board,spotActuelle,TOP, joueur,gaddag,  save);
     }
 }
+
+
 */
 
 #include "game.hpp"
