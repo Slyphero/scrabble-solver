a3eb424d4d79d61e0e7775ee9667634a4e700a8a
DUSSART--RABILLOUD BASTIEN p2204374 <p2204374@B761PC1208.univ-lyon1.fr>
2025-04-01 14:19:52 +0200

    ajout pseudo algo

--- src/game.cpp
+++ src/game.cpp
@@ -7,4 +7,39 @@ parcours gauche ou haut avant + on essaie de placer les lettres
 parcours droite ou bas après + on teste de placer les lettre du joueur
 */
 
+/*
+coup(Board currentBoard, Spot currentLocation, Direction currentDirection, Player player, struct save){
+    if (Board(currentLocation) == 0){
+        while((0 <= l || l >= 14) && (0<= c || c >= 14)){
+            for(int i = 0; i < player.inv.size(); i++){
+                verifier si gaddag[lettreJouer] existe{
+                    save.letter += score lettre * coef case
+                    save.multiple *= multiple mot
+                    if (gaddag est terminal){
+                        save état courant
+                        save.score *= save.multiple;
+                    }
+                    coup(Boardmodif, currentLocation + direction, currentDirection, player.inv - lettre jouer, save);
+                }
+            }
+        }  
+    }
+    else {
+        save.letter += score lettre * coef case
+        save.multiple *= multiple mot
+        coup(currentBoard, currentLocation + direction, currentDirection, player, save);
+    }
+    if (Gaddag["+"] existe){
+        coup (currentBoard, save.location, !direction, player, save);
+    }
+}
+
+autrefonction(){
+    parcours le tab de (8,8) en allant sur les voisin non vide{
+        coup(Board,spotActuelle,LEFT, joueur, save);
+        coup(Board,spotActuelle,TOP, joueur, save);
+    }
+}
+*/
+
 #include "game.hpp"
