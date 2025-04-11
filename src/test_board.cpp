09a05ccdc81ed68398fbf42296f687faac5ec796
Bastien Dussart--Rabilloud <bastien.dussart--rabilloud@etu.univ-lyon1.fr>
2025-04-12 00:01:09 +0200

    feat: test function in test_board

--- src/test_board.cpp
+++ src/test_board.cpp
@@ -6,6 +6,44 @@
 #include "game.hpp"
 #include "letters_collection.hpp"
 
+void testScore(Game game, Board board){
+  Position pos(7, 7);
+  std::cout<<"le score total du coup est : "<<game.scoreAll(board, VERTICAL, pos)<<std::endl;
+}
+
+void testPossible(Player player, Gaddag& gaddag, Position pos){
+  Board board;
+
+  std::stringstream ss;
+  ss << "....LOVA......." << std::endl;
+  ss << ".......R......." << std::endl;
+  ss << ".......B......." << std::endl;
+  ss << ".......R......." << std::endl;
+  ss << ".......E......." << std::endl;
+  ss << "..............." << std::endl;
+  ss << "..........U...." << std::endl;
+  ss << "....PROJETS...." << std::endl;
+  ss << ".......OT.U...." << std::endl;
+  ss << ".......U..R...." << std::endl;
+  ss << ".......E......." << std::endl;
+  ss << ".......UW......" << std::endl;
+  ss << ".....SCRABBLE.." << std::endl;
+  ss << "..............." << std::endl;
+  ss << "..............." << std::endl;
+
+  //XLine7 nom de la lettre placer ainsi que ça ligne
+  board.load(ss);
+  Game game(player, &gaddag, board, pos);
+  Position TLine7(8,8);
+  std::cout<<"test sur le coup Vertical non valide : "<<game.isPossible(board, VERTICAL, TLine7)<<std::endl;
+  Position WLine11(11,8);
+  std::cout<<"test sur le coup horrizontale non valide : "<<game.isPossible(board, HORIZONTAL, WLine11)<<std::endl;
+  Position SLine7(7,10);
+  std::cout<<"test sur le coup Vertical valide : "<<game.isPossible(board, VERTICAL, SLine7)<<std::endl;
+  Position ALine0(0,7);
+  std::cout<<"test sur le coup horrizontale valide : "<<game.isPossible(board, HORIZONTAL, ALine0)<<std::endl;
+}
+
 int main()
 {
   Board board;
@@ -46,6 +84,9 @@ int main()
   Position initialPosition(7, 9);
 
   Game game(player, &gaddag, board, initialPosition);
+
+  testScore(game,board);
+  testPossible(player, &gaddag, initialPosition);
 
   game.getPossibleNextStates(initialPosition, VERTICAL);
 
